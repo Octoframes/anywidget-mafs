060f0b73e09d1cbf60621b6106f0dac634a9f3c6
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-07-17 19:21:28 +0200

    add mafs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import QRCode from "react-qr-code";
+import { Mafs, Coordinates } from "mafs"
+
+import "mafs/core.css";
+import "mafs/font.css";
 
 export default function App() {
   return (
-    <>
-      <h1>Hello Anywidget + Vite!</h1>
-      <QRCode value="Heyyyhoo" />
-    </>
-  );
+    <Mafs>
+      <Coordinates.Cartesian />
+    </Mafs>
+  )
 }
