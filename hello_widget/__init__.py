bc2597f7a76637d06071989a2365ae328d7c2b6c
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-07-21 15:33:32 +0200

    swich from vite to esbuild

--- hello_widget/__init__.py
+++
@@ -1,8 +0,0 @@
-import pathlib
-import anywidget
-import traitlets
-
-class Mafs(anywidget.AnyWidget):
-    p = pathlib.Path(__file__).parent / "static/widget.js"
-    _esm =  p
-    _css=  pathlib.Path(__file__).parent / "static/style.css"
