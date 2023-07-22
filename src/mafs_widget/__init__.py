e62f64acd970b4da4ad91eed76a5f7ab80406630
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-07-22 10:49:06 +0200

    restructure

--- src/mafs_widget/__init__.py
+++ src/mafs_widget/__init__.py
@@ -4,6 +4,5 @@ import traitlets
 
 class Mafs(anywidget.AnyWidget):
     content = traitlets.Unicode("Hi").tag(sync=True)
-    _esm =  pathlib.Path(__file__).parent / "static/widget.js"
-
-    _css=  pathlib.Path(__file__).parent / "static/widget.css"
+    _esm =  pathlib.Path(__file__).parent / "widget.js"
+    _css=  pathlib.Path(__file__).parent / "widget.css"
